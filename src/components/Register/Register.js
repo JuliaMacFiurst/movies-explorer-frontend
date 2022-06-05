2585703e00eb1bee57490762ca3ab3ec2dd600ed
Julia Makhlin <tourdefiurst@yandex.com>
2022-06-05 16:32:57 +0300

    feat/add browserRouter

---
+++ src/components/Register/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Register() {
+    return (
+    <section className="register">
+      <div className="register-container">
+      </div>
+    </section>
+);
+    }
---
+++ src/components/SavedMovies/SavedMovies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SavedMovies() {
+    return(
+        <section className="saved_movies"></section>
+    )
+}
