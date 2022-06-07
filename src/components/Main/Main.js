e05f1ee166f5383f9ea50f6323813ca08fd11fda
Julia Makhlin <tourdefiurst@yandex.com>
2022-06-07 23:45:47 +0300

    feat/add techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,42 +4,46 @@ import Logo from "../Logo/Logo";
 import { NavLink } from "react-router-dom";
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
-
+import Techs from "../Techs/Techs";
 
 import "./Main.css";
 
 export default function Main() {
+  const isAuthorized = false;
 
-    const isAuthorized = false;
+  return (
+    <div className="main__container">
+      {isAuthorized ? (
+        <Header />
+      ) : (
+        <header className="main__auth-header">
+          <Logo />
+          <ul className="main__auth-list">
+            <li className="main__auth-item">
+              <NavLink
+                to="/signup"
+                className="main__auth-link main__auth-link_type_register"
+              >
+                Регистрация
+              </NavLink>
+            </li>
+            <li className="main__auth-item">
+              <NavLink
+                to="/signin"
+                className="main__auth-link main__auth-link_type_login"
+              >
+                Войти
+              </NavLink>
+            </li>
+          </ul>
+        </header>
+      )}
 
-    return (
-       <div className="main__container">
-           {isAuthorized ? 
-                <Header />
-                :
-                <header className="main__auth-header">
-                    <Logo />
-                    <ul className="main__auth-list">
-                        <li className="main__auth-item">
-                            <NavLink to="/signup"
-                            className="main__auth-link main__auth-link_type_register">
-                                Регистрация
-                            </NavLink>
-                        </li>
-                        <li className="main__auth-item">
-                        <NavLink to="/signin"
-                            className="main__auth-link main__auth-link_type_login">
-                                Войти
-                            </NavLink>
-                        </li>
-                    </ul>
-                </header>
-        }
-
-        <main className="main">
+      <main className="main">
         <Promo />
         <AboutProject />
-        </main>
-       </div>
-    );
+        <Techs />
+      </main>
+    </div>
+  );
 }
