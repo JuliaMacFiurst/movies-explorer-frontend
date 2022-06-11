e9297388d9b2ad19fd57686f0f872c85a3c71b81
Julia Makhlin <tourdefiurst@yandex.com>
2022-06-12 01:18:28 +0300

    feat/add savedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./SavedMovies.css";
+import { CurrentSavedCardsContext } from "../../context/CurrentSavedCardsContext";
+import Header from "../Header/Header";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import SearchForm from "../SearchForm/SearchForm";
+import Footer from "../Footer/Footer";
 
 export default function SavedMovies() {
-    return(
-        <section className="saved_movies"></section>
-    )
+  const savedMovies = useContext(CurrentSavedCardsContext);
+  return (
+    <>
+      <section className="saved_movies">
+        <SearchForm />
+        <MoviesCardList movies={savedMovies} />
+      </section>
+      <Footer />
+    </>
+  );
 }
